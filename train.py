8d6ee1a8f5b699f25944b58f1eadf930cdca17eb
avinash <avinash@avinashs-MacBook-Pro.local>
2024-12-31 09:09:24 -0800

    Reverted image augmentation adjustment and revert back to previous optimizer. Accuracy observed: 99.31%

--- train.py
+++ train.py
@@ -77,8 +77,8 @@ if __name__ == '__main__':
 
     # Train Phase transformations
     train_transforms = transforms.Compose([
-        transforms.RandomRotation((-7.0, 7.0), fill=(1,)),
-        transforms.RandomAffine(degrees=0, translate=(0.12, 0.12), scale=(0.95, 1.05)),
+        transforms.RandomRotation((-10.0, 10.0), fill=(1,)),
+        transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1)),  # Add this
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
     ])
